de78ce289277b872db5473e13c6c8a0d846d812c
PauGilArr <paugilarr@alu.edu.gva.es>
2024-02-05 16:06:20 +0100

    Creados controladores y modelos de las tablas

--- esportsTeam/app/Models/User.php
+++ esportsTeam/app/Models/User.php
@@ -20,7 +20,9 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'birthday',
         'password',
+        'rol',
     ];
 
     /**
@@ -42,4 +44,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function events() {
+        return $this->belongsToMany(Subject::class);
+    }
 }
