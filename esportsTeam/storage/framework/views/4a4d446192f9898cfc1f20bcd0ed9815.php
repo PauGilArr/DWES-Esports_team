968f208ccd366084b409892e26ef114c52055c0e
TorchEagle <10502068@ieselgrao.org>
2024-02-22 13:19:50 +0100

    Eliminada carpeta antigua del proyecto.

--- esportsTeam/storage/framework/views/4a4d446192f9898cfc1f20bcd0ed9815.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Proyectos de Laravel\DWES-Esports_team\esportsTeam\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
--- esportsTeam/storage/framework/views/fdbbc91dfcc3e07bb0af58104ebf30e9.php
+++
@@ -1,2 +0,0 @@
-<img src="/images/teamlogo.svg" alt="Sahara Force India F1 Team Logo">
-<?php /**PATH D:\Proyectos de Laravel\DWES-Esports_team\esportsTeam\resources\views/partials/teamlogo.blade.php ENDPATH**/ ?>
